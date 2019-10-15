dad7cf48f3e5bc1ee5f451686f61b1d53a4c6183
thejbte <thejbte@gmail.com>
2019-10-15 13:45:48 -0500

    Fw Driver, falta test y probar

---
+++ src/main/Max31855.c
@@ -0,0 +1,83 @@
+#include "Max31855.h"
+
+/**/
+
+/*
+*@brief Function for initialize driver
+*@param param1 : Data struct containing all data of driver
+*@param param2 : pointer to function SPI_ExchangeBytes of shif register SPI peripheral
+*@param param3 : pointer to function SPI chip selector or slave selector
+*
+*@return void
+*/
+void MAX31855_Init(Max31855_Data_t *obj, SPI_ExchangeByte_t SPI_TransferByte, SPI_CS_SS_t SPI_cs_ss){
+    obj->SPI_RW_Byte = SPI_TransferByte;
+    obj->SPI_CS_SS = SPI_cs_ss;
+}
+
+
+/*
+*@brief Function static for Read data of spi
+*@param param1 : Data struct containing all data of driver
+*@return void
+*/
+void MAX31855_ReadData(Max31855_Data_t *obj){
+  uint8_t DataW = 0xff;  /*Device is only read*/
+  uint8_t spival[4];
+  int8_t i;
+
+  obj->TempDataRegFull =0x0000;
+  obj->SPI_CS_SS(MAX31855_FALSE); /*CS=0 */
+  
+  for(i = MAX_NUM_BYTES_EXCHANGE - 1  ; i >= 0 ; i--)
+    spival[i] = obj->SPI_RW_Byte(DataW); /*Return byte of shif register SPI*/
+  
+  obj->SPI_CS_SS(MAX31855_TRUE);  /*CS=1*/
+  
+  for(i = 0 ; i < MAX_NUM_BYTES_EXCHANGE ; i++)
+    obj->TempDataRegFull |= spival[i] << (i*8) ;  
+}
+
+/*
+*@brief This Function for reads data  temperature external - Thermocouple temperature
+*@param param1 : Data struct containing all data of driver
+*@return Thermocouple temperature in °C  0.25°C to 1024°C
+*/
+int16_t MAX31855_ReadExtTemp(Max31855_Data_t *obj){
+  int16_t value;
+  
+  MAX31855_ReadData(obj);
+  
+    /* Bits D[31:18] are the signed 14-bit thermocouple temperature value*/
+  if (obj->TempDataRegFull & ((uint32_t)1 << 31)) { /* Sign extend negative numbers*/
+      value = ((obj->TempDataRegFull >> 18) & 0x3FFF); 
+      value = ( (~ (value ) & 0x3fff ) + 0x01 ) / (-4);     /*Complemento a 2*/ 
+  } else {
+      value = (obj->TempDataRegFull & 0x3fff) >> 18; /* Shift off all but the temperature data*/
+      value /= 4 ;
+    }
+  
+  return (value); /*Data>>2*/
+}
+
+/*
+*@brief This function reads the cold junction temperature
+*@param param1 : Data struct containing all data of driver
+*@return reference juntion temperature in °C 0.0625°C to 64°C
+*/
+int16_t MAX31855_ReadIntTemp(Max31855_Data_t *obj){
+  int16_t value;
+  
+  MAX31855_ReadData(obj);
+  
+  /* Bits D[31:18] are the signed 14-bit thermocouple temperature value*/
+  if (obj->TempDataRegFull & ((uint32_t)1 << 15)) { /* Sign extend negative numbers*/
+      value = ((obj->TempDataRegFull >> 4) & 0xFFF); 
+      value = ( (~ (value ) & 0xfff ) + 0x01 ) / (-16);     /*Complemento a 2   - 2^-4*/ 
+  } else {
+      value = (obj->TempDataRegFull & 0xfff) >> 4; /* Shift off all but the temperature data*/
+      value /= 4 ;
+    }
+  
+  return (value); /*Data>>2*/
+}
---
+++ src/main/Max31855.h
@@ -0,0 +1,99 @@
+#ifndef _MAX31855_H_
+#define _MAX31855_H_
+
+#include <stdint.h>
+
+#define MAX31855_TRUE 1u
+#define MAX31855_FALSE 0u
+#define MAX_NUM_BYTES_EXCHANGE 4   /*Register 32 bits*/
+
+/*@brief : pointer to function for read 8 bits of shif register Spi*/
+typedef uint8_t (*SPI_ExchangeByte_t)(uint8_t);
+
+/*@brief : pointer to function chip select or slave select Spi*/
+typedef void (*SPI_CS_SS_t)(uint8_t);   
+
+/*@brief struct containing all data*/
+typedef struct {
+    union{
+        uint32_t TempDataRegFull;       /*!< Register 4 bytes Spi*/
+        struct {
+           unsigned Fault_OC        :1; /*!< This bit is a 1 when the thermocouple is open(no connections). Default value is 0.*/
+           unsigned Fault_SCG       :1; /*!< This bit is a 1 when the thermocouple is short-circuited to GND. Default value is 0.*/
+           unsigned Fault_SCV       :1; /*!< This bit is a 1 when the thermocouple is short-circuited to VCC. Default value is 0.*/
+           unsigned Reserved_1      :1; /*!< This bit always reads 0.*/
+           unsigned TempInt_Raw     :12;/*!< These bits contain the signed 12-bit value of the reference junction temperature*/
+           unsigned Fault           :1; /*!< This bit reads at 1 when any of the SCV, SCG, or OC faults are active. Default values 0*/
+           unsigned Reserved_2      :1; /*!< This bit always reads 0.*/
+           unsigned TempExt_Raw     :14;/*!< These bits contain the signed 14-bit thermocouple temperature value*/
+        };
+        
+    };
+    SPI_ExchangeByte_t SPI_RW_Byte; /*!< Function for read 8 bits of shif register Spi*/
+    SPI_CS_SS_t SPI_CS_SS;          /*!< Function chip selector or slave selector spi*/
+}Max31855_Data_t;
+
+
+/*
+*@brief Function for initialize driver
+*@param param1 : Data struct containing all data of driver
+*@param param2 : pointer to function SPI_ExchangeBytes of shif register SPI peripheral
+*@param param3 : pointer to function SPI chip selector or slave selector
+*
+*@return void
+*/
+
+void MAX31855_Init(Max31855_Data_t *obj, SPI_ExchangeByte_t SPI_TransferByte, SPI_CS_SS_t SPI_cs_ss);
+/*
+*@brief Function static for Read data of spi
+*@param param1 : Data struct containing all data of driver
+*@return void
+*/
+void MAX31855_ReadData(Max31855_Data_t *obj);
+
+/*
+*@brief Function for Read data  temperature external - Thermocouple temperature
+*@param param1 : Data struct containing all data of driver
+*@return Thermocouple temperature in °C  0.25°C to 1024°C
+*/
+int16_t MAX31855_ReadExtTemp(Max31855_Data_t *obj);
+
+
+/*
+*@brief Function for Read data  temperature internal- reference juntion temperature 
+*@param param1 : Data struct containing all data of driver
+*@return reference juntion temperature in °C 0.0625°C to 64°C
+*/
+int16_t MAX31855_ReadIntTemp(Max31855_Data_t *obj);
+
+
+/*
+@see https://datasheets.maximintegrated.com/en/ds/MAX31855.pdf
+The practical temperature ranges vary with the thermocouple type.
+TEMPERATURE                                 TEMPERATURE
+(°C)                                           (°C)
+DIGITAL OUTPUT                            DIGITAL OUTPUT
+(D[31:18])                                       (D[15:4])
++1600.00    0110 0100 0000 00       +127.0000    0111 1111 0000
++1000.00    0011 1110 1000 00       +100.5625    0110 0100 1001
++100.75     0000 0110 0100 11       +25.0000     0001 1001 0000
++25.00      0000 0001 1001 00        0.0000      0000 0000 0000
+0.00        0000 0000 0000 00       -0.0625      1111 1111 1111
+-0.25       1111 1111 1111 11       -1.0000      1111 1111 0000
+-1.00       1111 1111 1111 00       -20.0000     1110 1100 0000
+-250.00     1111 0000 0110 00       -55.0000     1100 1001 0000
+
+
+
+
+
+
+
+
+
+
+-55.0000 1100 1001 0000
+
+*/
+
+#endif
